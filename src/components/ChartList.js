43e88633f34d282010c7638645c25b5753bca3cd
Christopher Pearson <cpearson3140@gmail.com>
2020-06-16 20:52:22 +0100

    updated Chartlist file
    Signed-off-by: Christopher Pearson <cpearson3140@gmail.com>

---
+++ src/components/ChartList.js
@@ -0,0 +1,16 @@
+import React, {Fragment} from 'react';
+import Chart from './Chart.js'
+
+const ChartList = (props) => {
+
+  const commentNodes = props.chart.map((chart, index) => {
+    return <Chart chart={chart} key={index} />
+  });
+
+  return (
+    <Fragment>
+        {commentNodes}
+    </Fragment>
+  )
+}
+export default ChartList;
